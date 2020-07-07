3af103f30029c308ffa62b4cda7a06aa50c2a702
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-07-07 22:56:51 +0530

    [rashmi] Added: The functionality to roll a die and playing option by displaying each move

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -2,6 +2,25 @@
 
 echo "Welcome to snake and ladder"
 position=0
-roll=`expr $(( RANDOM % 7)) | grep -v 0`
 
+
+function playingOption()
+{
+roll=`expr $(( RANDOM % 7)) | grep -v 0`
 echo "The die rolled to a $roll"
+option=$(( RANDOM%3 ))
+if [ $option -eq 0 ]
+then
+	echo "No play, position = $position"
+	position=$position
+elif [ $option -eq 1 ]
+then
+	echo "You got a ladder! move by $roll forward"
+	position=$(( $position + $roll ))
+else
+	echo "You got a snake! move by $roll backwards"
+	position=$(( $position - $roll ))
+fi
+}
+
+playingOption
